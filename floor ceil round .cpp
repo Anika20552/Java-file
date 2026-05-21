f208fd2d11af299b1108fb75202182d4e2d4db33
Halima Kamal Anika <halimaanikabd@gmail.com>
2026-05-21 18:45:19 +0600

    Add files via upload

---
+++ floor ceil round .cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double A, B;
+    cin >> A >> B;
+
+    double x = A / B;
+
+    cout << "floor " << A << " / " << B << " = " << floor(x) << endl;
+    cout << "ceil " << A << " / " << B << " = " << ceil(x) << endl;
+    cout << "round " << A << " / " << B << " = " << round(x) << endl;
+
+    return 0;
+}
