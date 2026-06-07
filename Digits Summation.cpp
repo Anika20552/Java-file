d85cd6db040e88398280879b8dc031c60a2f20f3
Halima Kamal Anika <halimaanikabd@gmail.com>
2026-06-07 22:48:25 +0600

    Create Digits Summation.cpp

---
+++ Digits Summation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    long long x=13,y=12,sum,a,b;
+    cin>>x>>y;
+    a = (x%10);
+    b = (y%10);
+    sum = (a+b);
+    cout<<sum<<endl;
+ 
+ 
+return 0;
+}
