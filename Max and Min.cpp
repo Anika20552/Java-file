43a1d76315e9646d713eddc96bcbb6e7cd13fbda
Halima Kamal Anika <halimaanikabd@gmail.com>
2026-06-07 21:59:39 +0600

    Create Max and Min.cpp

---
+++ Max and Min.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    int mn = A;
+    int mx = A;
+
+    if (B < mn) mn = B;
+    if (C < mn) mn = C;
+
+    if (B > mx) mx = B;
+    if (C > mx) mx = C;
+
+    cout << mn << " " << mx;
+
+    return 0;
+}
